a3f8c4dafccd1ce71a42dc47a4f5b7fb9d4dc9b0
Lab Web <30175757+phantomlsh@users.noreply.github.com>
2026-06-12 11:44:07 -0500

    Update init

--- llmpu/__init__.py
+++ llmpu/__init__.py
@@ -1,5 +1,5 @@
 from .config import config
-from .register import read
+from .register import read, write
 from .executor import run
 from .llm import request
 
--- llmpu/llm.py
+++ llmpu/llm.py
@@ -2,24 +2,26 @@ import json
 from .config import config
 
 client = None
+types = None
 
 def gemini_request(prompt):
-    global client
+    global client, types
     if client is None:
         from google import genai
         client = genai.Client(api_key=config["api_key"])
+        types = genai.types
     res = client.models.generate_content(
         model=config["model"],
         config=types.GenerateContentConfig(
-            thinking_config=types.ThinkingConfig(thinking_level="high"),
+            thinking_config=types.ThinkingConfig(thinking_level="low"),
             response_mime_type="application/json",
-            response_schema=types.Schema(type=types.Type.OBJECT, required=["code"], properties={ "code": types.Schema(type=types.Type.STRING) }),
+            response_schema=types.Schema(type=types.Type.OBJECT, required=["python_code"], properties={ "python_code": types.Schema(type=types.Type.STRING) }),
             **config["llm_config"]
         ),
         contents=prompt
     )
     r = json.loads(res.text)
-    return r["code"]
+    return r["python_code"]
 
 def request(prompt):
     return gemini_request(prompt)
