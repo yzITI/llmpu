13c11f63894c711fe95c7cb34a974c33a502f83c
Phantomlsh <30175757+phantomlsh@users.noreply.github.com>
2026-08-23 18:22:38 -0500

    Fix _run

--- llmpu/runner.py
+++ llmpu/runner.py
@@ -1,9 +1,13 @@
-import inspect
+import inspect, ctypes
 from .register import read, write
 from .config import config
 
-def _run(_c): # share local & global to avoid nonlocal issue
-    exec(read(_c) if isinstance(_c, int) else _c, inspect.currentframe().f_back.f_locals | config["EXEC"])
+def _run(c): # share local & global to avoid nonlocal issue
+    caller_frame = inspect.currentframe().f_back
+    var = caller_frame.f_locals | config["EXEC"]
+    exec(read(c) if isinstance(c, int) else c, var)
+    caller_frame.f_locals.update({ k: v for k, v in var.items() if k not in config["EXEC"] })
+    ctypes.pythonapi.PyFrame_LocalsToFast(ctypes.py_object(caller_frame), ctypes.c_int(0))
 
 def run(c): # isolate local variables
-    _run(c)
+    exec(read(c) if isinstance(c, int) else c, config["EXEC"], {})
