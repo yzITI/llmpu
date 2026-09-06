a9356cd2503c63366180f6eccdf0323f27388630
Phantomlsh <30175757+phantomlsh@users.noreply.github.com>
2026-09-06 16:54:11 -0500

    Add deploy-web action

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -2,7 +2,12 @@ import adapter from '@sveltejs/adapter-static'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: { adapter: adapter() },
-  preprocess: [] 
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH || ''
+    }
+  },
+  preprocess: []
 }
 export default config
