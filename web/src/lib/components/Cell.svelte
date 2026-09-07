71920ba5414bf38fce92bb2dbeaf8195116d6bea
Phantomlsh <30175757+phantomlsh@users.noreply.github.com>
2026-09-06 21:41:24 -0500

    Update web

--- web/src/lib/components/Cell.svelte
+++ web/src/lib/components/Cell.svelte
@@ -4,10 +4,12 @@
     ['opacity-50', 'opacity-100'],
     ['bg-blue-500', 'bg-gray-500', 'bg-orange-500']
   ]
+  let display = $derived(Number(r).toString(16).toUpperCase())
+  let size = $derived(4 / display.length - 0.5 * Math.max(0, (3 - display.length) ** 2 ))
 </script>
 
 {#key r}
-  <button {onclick} class="w-10 h-10 cursor-pointer rounded-xs font-mono font-bold flex items-center justify-center m-0.5 transition-all {s.map((x, i) => classes[i][x]).join(' ')}">
-    <div>{Number(r).toString(16).toUpperCase()}</div>
+  <button {onclick} class="w-12 h-12 cursor-pointer rounded-xs font-mono font-bold flex items-center justify-center m-0.5 transition-all {s.map((x, i) => classes[i][x]).join(' ')}" style="font-size: {size}rem;">
+    <div>{display}</div>
   </button>
 {/key}
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,15 +1,20 @@
 <script>
+  import Swal from 'sweetalert2'
   import srpc from '$lib/utilities/srpc.js'
   import Cell from '$lib/components/Cell.svelte'
+  import { AIcon } from 'ace.svelte'
+  import { mdiUpload, mdiDownload, mdiRefresh } from '@mdi/js'
 
   const LS = window.localStorage, SS = window.sessionStorage
   let url = $state(LS.url || 'http://localhost:22222/')
   let dumpPath = $state(LS.dumpPath || 'dump.json')
   let registers = $state({}), last = $state({})
   let newRegister = $state(''), focus = $state(false)
-  let loading = $state(false)
+  let loading = $state(false), connected = $state(false)
 
   let rs = $derived([...new Set([...Object.keys(registers), ...Object.keys(last)])].map(Number).sort((a, b) => a - b))
+  $effect(() => LS.url = url)
+  $effect(() => LS.dumpPath = dumpPath)
 
   async function init () {
     srpc(url)
@@ -19,8 +24,16 @@
   init()
 
   async function readAll () {
+    if (loading) return
     last = JSON.parse(JSON.stringify(registers))
-    registers = await srpc.read_all()
+    loading = 'Loading registers...'
+    connected = true
+    try {
+      registers = await srpc.read_all()
+    } catch {
+      connected = false
+    }
+    loading = false
   }
   
   async function newRegisterKeyup (e) {
@@ -39,15 +52,44 @@
     if (r === focus) res[1] = 0
     return res
   }
+
+  async function load () {
+    if (loading || !connected) return
+    loading = 'Loading dump file...'
+    try {
+      await srpc.load(dumpPath)
+    } catch (e) {
+      Swal.fire('Error', e.toString(), 'error')
+    }
+    loading = false
+    await readAll()
+    last = JSON.parse(JSON.stringify(registers))
+  }
+
+  async function dump () {
+    if (loading || !connected) return
+    loading = 'Dumping...'
+    try {
+      await srpc.dump(dumpPath)
+    } catch (e) {
+      Swal.fire('Error', e.toString(), 'error')
+    }
+    loading = false
+  }
 </script>
 
 <div class="w-full h-screen min-w-[768px] flex">
   <div class="w-1/2 h-full bg-gray-700 text-white">
     <h2 class="m-4 mb-0 w-full flex items-center justify-between">
-      <b class="text-xl">Registers</b>
+      <div class="flex items-center">
+        <b class="text-xl">Registers</b>
+        <button class="cursor-pointer mx-2 transition-all hover:rotate-90" onclick={readAll}>
+          <AIcon path={mdiRefresh}></AIcon>
+        </button>
+      </div>
       <div class="font-mono mx-4 flex items-center">
         0x
-        <input class="outline-none w-20 block" bind:value={newRegister} placeholder="___" onkeyup={newRegisterKeyup}>
+        <input class="outline-none w-20 block" bind:value={newRegister} placeholder="______" onkeyup={newRegisterKeyup}>
       </div>
     </h2>
     <div class="flex flex-wrap items-start p-4 w-full">
@@ -57,12 +99,21 @@
     </div>
   </div>
   <div class="w-1/2 h-full">
-    <p>To be constructed</p>
-    <div>
-      <input bind:value={url} placeholder="Server URL" class="outline-none text-xs">
+    <div class="flex items-center justify-between p-4">
+      <div class="flex items-center">
+        <button class="cursor-pointer w-5 h-5 font-bold mr-2 rounded-full {(loading || !connected) ? 'bg-gray-500' : 'bg-green-500'}" onclick={init}></button>
+        <b>{ connected ? (loading || 'Idle') : 'Disconnected'}</b>
+      </div>
+      <input bind:value={url} placeholder="Server URL" class="outline-none block grow text-right">
     </div>
-    <div>
-      <input bind:value={dumpPath} placeholder="Dump Path" class="outline-none text-xs">
+    <div class="flex items-center mx-3">
+      <button class="cursor-pointer transition-all hover:text-blue-500" onclick={load}>
+        <AIcon path={mdiUpload} size="1.75rem"></AIcon>
+      </button>
+      <button class="mx-1 cursor-pointer transition-all hover:text-blue-500" onclick={dump}>
+        <AIcon path={mdiDownload} size="1.75rem"></AIcon>
+      </button>
+      <input bind:value={dumpPath} placeholder="Dump Path" class="outline-none block grow">
     </div>
   </div>
 </div>
